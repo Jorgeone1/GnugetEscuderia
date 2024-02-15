5a39e0a2f1206f115b674ff5c5b7c06027814bf6
Jorgeone1 <jorgeifpdam@gmail.com>
2024-02-15 11:21:42 +0100

    bruuum bruum bruum

---
+++ Brum Brumm bruuumm/Brum Brumm bruuumm/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EscuderiasNuget;
+
+
+ListaEscuderias list = new ListaEscuderias();
+Carrera ca = new Carrera();
+//Crea un objeto de cada escuderia
+Escuderia ferrari =  ca.RescuperarEscuderia(TiposEscuderia.Ferrari);
+Escuderia Mclaren = ca.RescuperarEscuderia(TiposEscuderia.McLaren);
+Escuderia Mercedes = ca.RescuperarEscuderia(TiposEscuderia.Mercedes);
+Escuderia Redbull = ca.RescuperarEscuderia(TiposEscuderia.RedBull);
+Escuderia Toro = ca.RescuperarEscuderia(TiposEscuderia.ToroRosso);
+//
+Escuderia[] escuderias = {ferrari, Mclaren, Mercedes, Redbull,Toro};
+String pilotoCa = "";
+
+for(int i = 0; i < escuderias.Length; i++)
+{
+    pilotoCa += "Escuderia: "+escuderias[i].Nombre+ ", Piloto: "+escuderias[i].Piloto + "\n";
+}
+Console.WriteLine(pilotoCa);
